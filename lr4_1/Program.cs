2c410772c87d5625490c7f3419f4a93ab03a7a85
Juliya Ipatova <inu07040@mail.ru>
2020-12-23 18:18:58 +0300

    add lr4

---
+++ lr4_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+namespace lr4_1
+{
+    class Program
+    {
+        private static readonly log4net.ILog log = LogHelper.GetLogger(); 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+
+            log.Error("this is error, u know?");
+            log.Info("info, but about?");
+            log.Debug("u run ur test, but u r happy?");
+            log.Warn("!!!WARNING!!! u lost ur life");
+            log.Fatal("u r late. ur happy leave us");
+
+            Console.ReadLine();
+        }
+    }
+}
